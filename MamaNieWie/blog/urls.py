245ff11ff282c64cb5d919e2904faebe692b4e98
ntmagda <ntmagda93@gmail.com>
2016-02-18 15:07:53 -0800

    initial version

---
+++ MamaNieWie/blog/urls.py
@@ -0,0 +1,7 @@
+__author__ = 'MagdaNT'
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.BlogIndex.as_view(), name='index'),
+]
---
+++ MamaNieWie/blog/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from django.views import generic
+from . import models
+
+class BlogIndex(generic.ListView):
+    queryset = models.Entry.objects.published()
+    template_name = "blog/index.html"
+    paginate_by = 2
